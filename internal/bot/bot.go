cbf5346e4e65ff8c485240b452d2b648b387e641
anton.kochetkov <anton.kochetkov@regium.com>
2021-11-12 02:23:10 +0300

    add checkLimitsOrders

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -24,12 +24,14 @@ const (
 )
 
 type Bot struct {
-	client       *binance.Client
-	analysisСhan chan *binance.WsAggTradeEvent
-	orderChan    chan *Order
-	Symbol       string // trading pair
-	lastTimeDeal int64  // unix time from last deal
-	stopPrice    float64
+	client         *binance.Client
+	analysisСhan   chan *binance.WsAggTradeEvent
+	orderChan      chan *Order
+	finalOrderChan chan *binance.CreateOrderResponse
+	Symbol         string // trading pair
+	lastTimeDeal   int64  // unix time from last deal
+	stopPrice      float64
+	lastDealPrice  float64
 
 	wg *sync.WaitGroup
 }
@@ -46,6 +48,7 @@ func NewBot(client *binance.Client, cfg *configs.BotConfig) *Bot {
 	bot.client = client
 	bot.analysisСhan = make(chan *binance.WsAggTradeEvent, sizeChan)
 	bot.orderChan = make(chan *Order, sizeChan)
+	bot.finalOrderChan = make(chan *binance.CreateOrderResponse, sizeChan)
 	bot.Symbol = cfg.Symbol
 	bot.stopPrice = stopPrice
 	bot.wg = &sync.WaitGroup{}
@@ -62,12 +65,16 @@ func (o *Bot) Start() {
 
 	go o.Trade()
 
+	o.wg.Add(1)
+
+	go o.CheckLimitOrder()
+
 	o.wg.Wait()
 
 	log.Println("Bot stop work")
 }
 
-func StartPricesStream(analysisСhan chan *binance.WsAggTradeEvent) chan struct{} {
+func (o *Bot) StartPricesStream(analysisСhan chan *binance.WsAggTradeEvent) chan struct{} {
 	wsAggTradeHandler := func(event *binance.WsAggTradeEvent) {
 		analysisСhan <- event
 	}
@@ -75,7 +82,7 @@ func StartPricesStream(analysisСhan chan *binance.WsAggTradeEvent) chan struct{
 		log.Println(err)
 	}
 
-	doneC, _, err := binance.WsAggTradeServe("BTCBUSD", wsAggTradeHandler, errHandler)
+	doneC, _, err := binance.WsAggTradeServe(o.Symbol, wsAggTradeHandler, errHandler)
 	if err != nil {
 		log.Println(err)
 		return nil
@@ -95,7 +102,7 @@ func (o *Bot) Analyze() {
 		log.Println(err)
 	}
 
-	doneC, _, err := binance.WsAggTradeServe("BTCBUSD", wsAggTradeHandler, errHandler)
+	doneC, _, err := binance.WsAggTradeServe(o.Symbol, wsAggTradeHandler, errHandler)
 	if err != nil {
 		log.Println(err)
 		return
@@ -197,29 +204,90 @@ func (o *Bot) Trade() {
 			continue
 		}
 
-		_, err := o.CreateOrder(order, binance.SideTypeBuy, binance.OrderTypeMarket, binance.TimeInForceTypeGTC)
+		if order.Price > o.lastDealPrice {
+			log.Printf("order: %v skip, order.Price(%v) > o.lastDealPrice(%v)\n", order, order.Price, o.lastDealPrice)
+			continue
+		}
+
+		depth, err := o.client.NewDepthService().Symbol(order.Symbol).
+			Do(context.Background())
+		if err != nil {
+			log.Printf("o.client.NewDepthService(%v) err: %v\n", order.Symbol, err)
+
+			continue
+		}
+
+		price, err := strconv.ParseFloat(depth.Bids[0].Price, 32)
+		if err != nil {
+			log.Printf("strconv.ParseFloat(depth.Bids[0].Price) err: %v\n", err)
+
+			continue
+		}
+
+		order.Price = price
+
+		orderResolve, err := o.CreateOrder(order, binance.SideTypeBuy, binance.OrderTypeLimitMaker, binance.TimeInForceTypeGTC)
 		if err != nil {
 			log.Printf("An error occurred during order execution, order: %v, err: %v\n", order, err)
 			continue
 		}
 
+		o.lastTimeDeal = time.Now().Unix()
 
-		log.Printf("Order %v executed\n", order)
+		go func(order *Order, orderResolve *binance.CreateOrderResponse) {
+			timeNow := time.Now().Unix()
 
-		for {
-			response, err := o.CreateOrder(order, binance.SideTypeSell, binance.OrderTypeLimit, binance.TimeInForceTypeGTC)
+			for {
+				orderTimeout := (time.Now().Unix() - timeNow)
 
-			if err != nil {
-				log.Printf("An error occurred during order execution, order: %v, type: %v, err: %v\n", order, binance.OrderTypeLimit, err)
-				continue
+				if orderTimeout > 100 {
+					_, err := o.client.NewCancelOrderService().Symbol(order.Symbol).
+						OrderID(orderResolve.OrderID).Do(context.Background())
+					if err != nil {
+						log.Println(err)
+						continue
+					}
+
+					log.Printf("order %v canceled after timeout", order)
+
+					return
+				}
+
+				order, err := o.client.NewGetOrderService().Symbol(order.Symbol).
+					OrderID(orderResolve.OrderID).Do(context.Background())
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+
+				if order.Status == binance.OrderStatusTypeFilled {
+					break
+				}
+
+				time.Sleep(time.Second * 2)
 			}
 
-			log.Printf("Order limit create %v\n", response)
+			log.Printf("Order %v executed\n", order)
 
-			break
-		}
+			order.Price += profit
 
-		o.lastTimeDeal = time.Now().Unix()
+			for {
+				response, err := o.CreateOrder(order, binance.SideTypeSell, binance.OrderTypeLimit, binance.TimeInForceTypeGTC)
+
+				if err != nil {
+					log.Printf("An error occurred during order execution, order: %v, type: %v, err: %v\n", order, binance.OrderTypeLimit, err)
+					continue
+				}
+
+				log.Printf("Order limit create %v\n", response)
+
+				o.finalOrderChan <- response
+
+				break
+			}
+
+			o.lastTimeDeal = time.Now().Unix()
+		}(order, orderResolve)
 	}
 }
 
@@ -240,17 +308,23 @@ func (o *Bot) CreateOrder(
 			Type(typeOrder).
 			Quantity(fmt.Sprintf("%f", order.Quantity)).
 			Do(context.Background())
+	case binance.OrderTypeLimitMaker:
+		orderResponse, err = o.client.NewCreateOrderService().Symbol(order.Symbol).
+			Side(side).
+			Type(typeOrder).
+			Quantity(fmt.Sprintf("%f", order.Quantity)).
+			Price(fmt.Sprintf("%.2f", order.Price)).
+			Do(context.Background())
 
 	case binance.OrderTypeLimit:
-		getProfit := int(order.Price + profit)
-
 		orderResponse, err = o.client.NewCreateOrderService().Symbol(order.Symbol).
 			Side(side).
 			Type(typeOrder).
 			TimeInForce(timeInForce).
 			Quantity(fmt.Sprintf("%v", order.Quantity)).
-			Price(fmt.Sprintf("%v", getProfit)).
+			Price(fmt.Sprintf("%.2f", order.Price)).
 			Do(context.Background())
+
 	case binance.OrderTypeStopLoss:
 		stopLoss := int(order.Price - profit)
 
@@ -266,6 +340,32 @@ func (o *Bot) CreateOrder(
 	return orderResponse, err
 }
 
+func (o *Bot) CheckLimitOrder() {
+	defer o.wg.Done()
+
+	for order := range o.finalOrderChan {
+		go func(order *binance.CreateOrderResponse) {
+			for {
+				order, err := o.client.NewGetOrderService().Symbol(order.Symbol).
+					OrderID(order.OrderID).Do(context.Background())
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+
+				if order.Status == binance.OrderStatusTypeFilled {
+					price, _ := strconv.ParseFloat(order.Price, 32)
+					o.lastDealPrice = price
+
+					return
+				}
+
+				time.Sleep(time.Second * 10)
+			}
+		}(order)
+	}
+}
+
 func (o *Bot) GetAccountInfo() func(http.ResponseWriter, *http.Request) {
 	return func(resWriter http.ResponseWriter, req *http.Request) {
 		res, err := o.client.NewGetAccountService().Do(context.Background())
@@ -277,7 +377,7 @@ func (o *Bot) GetAccountInfo() func(http.ResponseWriter, *http.Request) {
 		var notZeroAssets []binance.Balance
 
 		for _, ass := range res.Balances {
-			if ass.Asset == "BUSD" || ass.Asset == "BTC" {
+			if ass.Asset == "BUSD" || ass.Asset == "USDT" || ass.Asset == "BTC" {
 				notZeroAssets = append(notZeroAssets, ass)
 			}
 		}
